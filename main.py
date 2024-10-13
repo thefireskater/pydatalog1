4d435c5653d2c184cfeb8384f52009404aae78e5
thefireskater <114769322+thefireskater@users.noreply.github.com>
2024-10-13 11:21:32 -0600

    Simple usage of SQLite3

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from pyDatalog import pyDatalog
+import sqlite3
 
 # Symbols have to be declared ("bound")
 pyDatalog.create_terms('X,Y,Z,manager,works_in')
@@ -17,3 +18,15 @@ print()
 
 print('X manages Y')
 print(manager(X, Y))
+
+# creates a db if a db of this name doesn't exist
+connection = sqlite3.connect(':memory:')
+print(connection.total_changes) # ensure that the connection was successful
+
+cursor = connection.cursor()
+cursor.execute('CREATE TABLE person (name TEXT, age INTEGER)')
+cursor.execute("INSERT INTO person VALUES ('Alice', 30)")
+cursor.execute("INSERT INTO person VALUES ('Bob', 31)")
+
+rows = cursor.execute("SELECT name FROM person WHERE age < 31").fetchall()
+print(rows)
